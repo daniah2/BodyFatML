3c7b63a0012d71ce729653fea83c8f8c5cebb774
dani <daniahalmosa2@gmail.com>
2025-11-17 23:42:56 +0300

    Add all files to GitHub

---
+++ README.md
@@ -0,0 +1,8 @@
+# Body Fat & Muscle Prediction API
+
+## Overview
+This project provides an API for predicting **body fat percentage** and **muscle percentage** using a trained machine learning model. The API is built with **FastAPI** and can be run locally or via **Docker**. 
+
+It is designed to take user inputs for basic body measurements and return predictions for body composition.
+
+---
---
+++ fastAPI.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+
+
+MODEL_PATH = "BestModel.pkl"
+
+
+with open(MODEL_PATH, "rb") as f:
+    model = pickle.load(f)
+
+# FastAPI
+app = FastAPI(title="Body Fat & Muscle Prediction API")
+
+class BodyMeasurements(BaseModel):
+    Weight: float
+    Height: float
+    Chest: float
+    Abdomen: float
+    Hip: float
+    Thigh: float
+    Biceps: float
+
+
+@app.post("/predict")
+def predict(data: BodyMeasurements):
+    
+    BMI = round(data.Weight / (data.Height ** 2), 2)
+    Waist_Hip_Ratio = data.Abdomen / data.Hip
+    Chest_Height_Ratio = data.Chest / data.Height
+    Thigh_Height_Ratio = data.Thigh / data.Height
+
+    input_data = [[
+        data.Weight, data.Height, data.Chest, data.Abdomen,
+        data.Hip, data.Thigh, data.Biceps,
+        BMI, Waist_Hip_Ratio, Chest_Height_Ratio, Thigh_Height_Ratio
+    ]]
+
+    bodyfat_prediction = round(model.predict(input_data)[0], 2)
+    muscle_percentage = round(100 - bodyfat_prediction, 2)
+
+    return {
+        "Predicted_Body_Fat": bodyfat_prediction,
+        "Estimated_Muscle": muscle_percentage
+    }
